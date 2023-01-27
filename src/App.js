0e3bf4ca250a13e2518c08d408412e351fea395f
Gianluca Mazzeo <g.mazzeo@theinnovationfactory.it>
2023-01-27 09:29:59 +0100

    [signin] fix conflict

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import { Routes, Route } from 'react-router-dom';
 
@@ -20,28 +19,7 @@ const App = () => {
       </Route>
     </Routes>
   );
-=======
-import { Routes, Route } from 'react-router-dom';
-
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
->>>>>>> 10bcf06 ([nav] fix app nav)
 };
 
-const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
-      </Route>
-    </Routes>
-  );
-};
 
 export default App;
