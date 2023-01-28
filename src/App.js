1180afe0e87bf0ed1b514e9eb3dacff18f40a231
Gianluca Mazzeo <g.mazzeo@theinnovationfactory.it>
2023-01-28 13:15:06 +0100

    [fix]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 import { Routes, Route } from 'react-router-dom';
-
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
+import Directory from './components/directory/directory.component';
+
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -21,37 +20,11 @@ const App = () => {
       </Route>
     </Routes>
   );
-=======
-import Directory from './components/directory/directory.component';
-=======
-import { Routes, Route } from 'react-router-dom';
->>>>>>> 10bcf06 ([nav] fix app nav)
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
 
-<<<<<<< HEAD
-  return <Directory categories={categories} />;
->>>>>>> 0e7b438 (updating our directory page with directory component and category item component)
 };
 
-=======
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
 
-const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
-      </Route>
-    </Routes>
-  );
-};
->>>>>>> 10bcf06 ([nav] fix app nav)
+
 
 export default App;
